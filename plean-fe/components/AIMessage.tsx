6ae85c65f612d952836a4cdcd7ca7c996eae0cb2
saf <safwaanshaikh21@gmail.com>
2025-01-16 12:46:50 -0500

    Revert "setup+search bar"
    This reverts commit 64b84d3328ff1866fcfc08898a34b1e599437eed.

--- plean-fe/components/AIMessage.tsx
+++
@@ -1,5 +0,0 @@
-export default function AIMessage({message} : {message: string}) {
-    return (
-        <p>{message}</p>
-    )
-}
--- plean-fe/components/UserMessage.tsx
+++
@@ -1,8 +0,0 @@
-
-
-export default function UserMessage({message} : {message: string}) {
-    return (
-        <h1>{message}</h1>
-    )
-}
-    
