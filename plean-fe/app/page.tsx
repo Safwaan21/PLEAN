ba94be7e9ebb3c72f77803a125605d07011cd38a
saf <safwaanshaikh21@gmail.com>
2025-01-16 12:47:54 -0500

    Revert "Revert "setup+search bar""
    This reverts commit e5d925ea60e3969e6bd27af656821223f2a06128.

---
+++ plean-fe/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import SearchBar from "../components/SearchBar";
+
+export default function Home() {
+  return (
+    
+    <div className="flex  justify-center">
+      <SearchBar />
+    </div>
+  );
+}
---
+++ plean-fe/components/AIMessage.tsx
@@ -0,0 +1,5 @@
+export default function AIMessage({message} : {message: string}) {
+    return (
+        <p>{message}</p>
+    )
+}
