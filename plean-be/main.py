ffc4492247764855f226681788153af037892049
saf <safwaanshaikh21@gmail.com>
2025-01-17 19:51:38 -0500

    add some test data

--- plean-be/main.py
+++ plean-be/main.py
@@ -1,7 +1,55 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
+import time
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Your Next.js frontend origin
+    allow_credentials=True,  # Allow cookies or authentication
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all HTTP headers
+)
+
+mock_result = {
+    "num_results": "3",
+    "results": [
+        {
+        "type": "gdoc",
+        "link": "https://docs.google.com",
+        "name": "Document 1",
+        "owner": "John Doe",
+        "description": """
+        This is a document about the history of the internet.
+        This is a document about the history of the internet. 
+        This is a document about the history of the internet.
+        This is a document about the history of the internet.
+        This is a document about the history of the internet. 
+        This is a document about the history of the internet.
+        """,
+        "creation_date": "2021-09-01",
+        },
+        {
+        "type": "gdoc",
+        "link": "https://docs.google.com",
+        "name": "Document 2",
+        "owner": "Jane Smith",
+        "description": "This is a document about the history of the internet as well.",
+        "creation_date": "2021-09-02",
+        },
+        {
+        "type": "gslide",
+        "link": "https://slides.google.com",
+        "name": "Presentation 1",
+        "owner": "John Doe",
+        "description": "This is a presentation about the history of the internet.",
+        "creation_date": "2021-09-03",
+        }
+    ],
+}
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    time.sleep(2)
+    return {"search_results": mock_result}
