2ce1efa74ff469a768bad42c8a2e401bfdf5fbdc
saf <safwaanshaikh21@gmail.com>
2025-01-24 17:26:13 -0500

    fix constant variables server-side

--- plean-be/config.py
+++ plean-be/config.py
@@ -6,8 +6,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/userinfo.email",  
     "https://www.googleapis.com/auth/userinfo.profile",  
 ]
-REDIRECT_URI = "http://localhost:8000/auth/google/callback"
+REDIRECT_URI = os.getenv("REDIRECT_URL", "http://localhost:8000/auth/google/callback")
 CLIENT_SECRETS_FILE = "client_id.json"
-ALLOWED_ORIGINS = ["http://localhost:3000"]
-
+ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", ["http://localhost:3000"])
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./user_data.db")
